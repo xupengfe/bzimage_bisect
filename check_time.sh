48335560c790a19c3ce0ae51a843926992c89da5
Pengfei Xu <pengfei.xu@intel.com>
2021-08-11 15:36:51 +0800

    check_time.sh:add the script for reproduce time
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

---
+++ check_time.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+FILE=$1
+POINT=$2
+TIME=""
+
+check_time() {
+  local dmesg_file=$1
+  local result=""
+  local time=""
+
+  [[ -e "$dmesg_file" ]] || {
+    echo "dmesg_file:$dmesg_file is not exist, exit"
+    exit 1
+  }
+
+  result=$(cat $dmesg_file | grep "$POINT" | head -n 1)
+  [[ -n "$result" ]] || {
+    echo "No $POINT dmesg info:$result, exit"
+    exit 1
+  }
+
+  time=$(echo "$result" | awk -F " " '{print $2}' | cut -d '.' -f 1)
+  echo "Found time:$time!"
+  if [[ "$time" -le 20 ]]; then
+    TIME=20
+  elif [[ "$time" -le 60 ]]; then
+    TIME=time
+  else
+    TIME=$((time+60))
+  fi
+  echo "Set TIME:$TIME"
+}
+
+[[ -n "$POINT" ]] || {
+  echo "POINT:$POINT is null, exit"
+  exit 1
+}
+
+check_time "$FILE"
