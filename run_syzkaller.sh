36c5afe1ad1a27f14b4da5009922844953f2e39c
Pengfei Xu <pengfei.xu@intel.com>
2021-08-18 09:37:18 +0800

    run_syzkaller.sh: for git update and screen usage
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

---
+++ run_syzkaller.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0
+# Just call update.sh and run_syz.sh
+
+export PATH=${PATH}:/root/bzimage_bisect
+UPDATE_LOG="/root/update_bzimage_bisect.log"
+
+TAG=$1
+
+update.sh
+echo "run_syz.sh -t $tag" >> "$UPDATE_LOG"
+run_syz.sh -t "$TAG"
