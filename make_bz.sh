04b2c521f85b88f950458601286912b95204f567
Pengfei Xu <pengfei.xu@intel.com>
2021-08-22 13:18:49 +0800

    Correct the bad_commit judgement
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

--- make_bz.sh
+++ make_bz.sh
@@ -84,7 +84,7 @@ prepare_kconfig() {
   do_cmd "cp -rf ${KCONFIG_NAME}_kvm .config"
   print_log "git checkout -f $COMMIT" "$STATUS"
   do_cmd "git checkout -f $COMMIT"
-  if [[ -z "$bad_commit" ]]; then
+  if [[ -n "$bad_commit" ]]; then
     print_log "There was bad commit:$bad_commit, will revert it" "$STATUS"
     do_cmd "git show $bad_commit | head -n 20"
     git revert -n $bad_commit
