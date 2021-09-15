e2b1f5a3b653446712a2e305dfb12df2222dd36f
Pengfei Xu <pengfei.xu@intel.com>
2021-09-15 14:55:53 +0800

    update.sh: add proxy in /etc/environment
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

--- update.sh
+++ update.sh
@@ -5,6 +5,7 @@
 RUNSYZ_FOLDER="/root/bzimage_bisect"
 UPDATE_LOG="/root/update_bzimage_bisect.log"
 
+source /etc/environment
 cd "$RUNSYZ_FOLDER"
 date +%Y-%m-%d_%H:%M:%S >> "$UPDATE_LOG"
 git pull >> "$UPDATE_LOG"
